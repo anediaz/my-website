622b05e109cb18969e11883b57ccde38e78292a1
Ane Diaz de Tuesta <ane.diazdetuesta@datadoghq.com>
2023-08-31 09:37:44 +0200

    Add giphy tag to animations page

--- src/screens/Animations/Animations.js
+++ src/screens/Animations/Animations.js
@@ -11,25 +11,34 @@ const Animations = () => {
     <div className="Animations">
       <div className="title" />
       <div className="container">
-        <div className="stickers">
-          <div className="subtitle">
-            <i className="fa fa-angle-double-right symbol" aria-hidden="true" />
-            Stickers
+        <div className="description">
+          <div />
+          <div>
+            Find me on GIPHY
+            <a href="https://giphy.com/ane_naiz" target="_blank" rel="noreferrer">@ane_naiz</a>
           </div>
-          <Gallery
-            photos={stickers || []}
-            configurations={animationsConfigurations}
-          />
         </div>
-        <div className="gifs">
-          <div className="subtitle">
-            <i className="fa fa-angle-double-right symbol" aria-hidden="true" />
-            Gifs
+        <div className="media">
+          <div className="stickers">
+            <div className="subtitle">
+              <i className="fa fa-angle-double-right symbol" aria-hidden="true" />
+              Stickers
+            </div>
+            <Gallery
+              photos={stickers || []}
+              configurations={animationsConfigurations}
+            />
           </div>
-          <Gallery
-            photos={gifs || []}
-            configurations={animationsConfigurations}
-          />
+          <div className="gifs">
+            <div className="subtitle">
+              <i className="fa fa-angle-double-right symbol" aria-hidden="true" />
+              Gifs
+            </div>
+            <Gallery
+              photos={gifs || []}
+              configurations={animationsConfigurations}
+            />
+          </div>
         </div>
       </div>
     </div>
