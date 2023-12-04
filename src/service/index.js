6b0b8333536ecc3fd08f352f5afc2102bf343fab
Ane Diaz de Tuesta <ane.diazdetuesta@datadoghq.com>
2023-12-04 06:57:24 +0100

    ♻️ Media constants: migrate them to ts

--- src/service/index.js
+++ src/service/index.js
@@ -1,4 +1,3 @@
 export * as articleData from './articles';
 export * as jsonData from './data.json';
-export { default as microsoftMedia } from './microsoftMedia';
-export { default as paquierMedia } from './paquierMedia';
+export * from './media.constants';
--- src/service/media.constants.ts
+++ src/service/media.constants.ts
@@ -1,4 +1,11 @@
-const microsoftMedia = [
+interface MediaProps {
+  title: String;
+  alt: String;
+  tag: String;
+  youtubeId?: String;
+}
+
+export const microsoftMedia:MediaProps[] = [
   {
     title: 'Inclusive Design is for everyone',
     youtubeId: 'ISjbFDcGqhA',
@@ -85,4 +92,40 @@ const microsoftMedia = [
   },
 ];
 
-export default microsoftMedia;
+export const paquierMedia:MediaProps[] = [
+  {
+    title: 'Peps',
+    alt: 'peps',
+    tag: 'paquier01',
+  },
+  {
+    title: 'Anticipation',
+    alt: 'anticipation',
+    tag: 'paquier02',
+  },
+  {
+    title: 'Qualité',
+    alt: 'qualité',
+    tag: 'paquier03',
+  },
+  {
+    title: 'Unité',
+    alt: 'unité',
+    tag: 'paquier04',
+  },
+  {
+    title: 'Intégrité',
+    alt: 'intégrité',
+    tag: 'paquier05',
+  },
+  {
+    title: 'Engagement',
+    alt: 'engagement',
+    tag: 'paquier06',
+  },
+  {
+    title: 'Respect',
+    alt: 'respect',
+    tag: 'paquier07',
+  },
+];
